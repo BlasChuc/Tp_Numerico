ff462e8bcb228aa8f750f6b372b5c44691172827
BlasChuc <bchuc@fi.uba.ar>
2025-06-23 14:36:33 -0300

    recta 1 + curva 1

--- Ejecucion_trayectoria.py
+++ Ejecucion_trayectoria.py
@@ -5,9 +5,13 @@ import matplotlib.image as mpimg
 # Cargar imagen de fondo
 img = mpimg.imread("Captura de pantalla de 2025-06-2(1).png")
 
-F = 0 # Suponiendo una fuerza constante a lo largo de toda la recta
+#F = 0 # Suponiendo una fuerza constante a lo largo de toda la recta
 
-def ecuacion_recta(tiempo, posicion, velocidad):
+X_INI = 2
+Y_INI = 14
+
+def ecuacion_recta(tiempo, posicion, velocidad, *args):
+    F = args[0] 
     if F > FUERZA_MAX:
         return 0
     return F / M # Retorna la aceleración (Fuerza / Masa)
@@ -15,64 +19,54 @@ def ecuacion_recta(tiempo, posicion, velocidad):
 def calcular_velocidad_resultante(v_final_x, v_final_y):
      return np.sqrt(v_final_x**2 + v_final_y**2)
 
-
-
-def calcular_tiempo_total(r1, r2, v_inicial_x, v_inicial_y, d1, d2, pos_inicial_x, pos_inicial_y, t_lim_1):
+def calcular_tiempo_total(r1, r2, v_inicial_x, v_inicial_y, d1, d2, t_lim, F1, F2):
     x_total = []
     y_total = []    
     h = 0.1
-    ang1, ang2 = np.pi / 2, -np.pi / 4  # Ángulos de giro en radianes
+    t0 = 0.0
+    ang1, ang2 = np.pi/2, -np.pi / 4  # Ángulos de giro en radianes
 
+    args_recta_1 = (F1)
 
-    # Recta 1 (en eje x)
-    """ t_x_1, pos_x_1, v_x_1 = runge_kutta_4_orden_superior(ecuacion_recta, 0.0, pos_inicial_x, v_inicial_x, t_lim_1, h, d1)
-    v_final_x = v_x_1[-1]
-    x_total += pos_x_1 """
+    # Recta 1
+    #xs1, ys1, vxs1, vys1, t1 = runge_kutta_4_orden_superior_2d(ecuacion_recta, t0, X_INI, Y_INI, v_inicial_x, v_inicial_y, t_lim, h, d1, args_recta_1)
+    xs1, ys1, vxs1, vys1, t1 = simular_tramo_recto(t0, X_INI, Y_INI, v_inicial_x, v_inicial_y, t_lim, h, d1, args_recta_1)
+    v_final_x_1 = vxs1[-1]
+    v_final_y_1 = vys1[-1]
 
-    xs, ys, vxs, vys, t = runge_kutta_4_orden_superior_2d(ecuacion_recta, 0.0, pos_inicial_x, pos_inicial_y, v_inicial_x, v_inicial_y, t_lim_1, h, d1)
-    v_final_x = vxs[-1]
-    x_total += xs
-    y_total += ys
+    x_total += xs1
+    y_total += ys1
 
+    args_curva_1 = (ang1, r1, 1000)
 
-    # Recta 1 (en eje y)
-    """ t_y_1, pos_y_1, v_y_1 = runge_kutta_4_orden_superior(ecuacion_recta, 0.0, pos_inicial_y, v_inicial_y, t_lim_1, h, d2)
-    v_final_y = v_y_1[-1]
-    y_total += pos_y_1
- """
-    v_antes_curva1 = calcular_velocidad_resultante(vxs[-1], vys[-1])
-    print("Tiempo final en el eje X:", xs[-1])
-    print("Tiempo final en el eje Y:", ys[-1])
+    # Curva 1
+    #xs2, ys2, vxs2, vys2, t2 = runge_kutta_4_orden_superior_2d(ecuacion_curvas, t1, xs1[-1], ys1[-1], v_final_x_1, v_final_y_1, t_lim, h, d2, args_curva_1)
+    xs2, ys2, vxs2, vys2, t2 = simular_tramo_curvo(t1, xs1[-1], ys1[-1], v_final_x_1, v_final_y_1, t_lim, h, d2, args_curva_1)
 
-    
-
-    # Curva 1 
-    #t_x_2, pos_x_2, v_x_2 = runge_kutta_4_orden_superior(ecuacion_curvas, pos_x_1[-1], v_antes_curva1, h, pos_x_1[-1], r1, ang1)
-
-    #tiempo_curva2 = tiempo_curva(r2, ang2)
+    x_total += xs2
+    y_total += ys2
 
     return x_total, y_total
 
 
 r1 = 20  # Radio de la curva 1
 r2 = 30  # Radio de la curva 2
-v_inicial_x = 50.0  # Velocidad inicial en el eje X (m/s)
+v_inicial_x = 40.0  # Velocidad inicial en el eje X (m/s)
 v_inicial_y = -2.5   # Velocidad inicial en el eje Y (m/s)
-d2 = -2
+d2 = 50
 
-x_ini, y_ini = 2, 14
-x_fin, y_fin = 69, 9
+x_fin, y_fin = 71, 10
 
-dx = x_fin - x_ini
-dy = y_fin - y_ini
+dx = x_fin - X_INI
+dy = y_fin - Y_INI
 d_lim_recta_1 = np.sqrt(dx**2 + dy**2)
 
+t_lim = 6
 
-pos_inicial_x = 2.0  # Posición inicial en el eje X (m)
-pos_inicial_y = 14.0  # Posición inicial en el eje Y (m)
-t_lim_1 = 2
+f1 = 0
+f2 = 0
 
-x_total, y_total = calcular_tiempo_total(r1, r2, v_inicial_x, v_inicial_y, d_lim_recta_1, d2, pos_inicial_x, pos_inicial_y, t_lim_1)
+x_total, y_total = calcular_tiempo_total(r1, r2, v_inicial_x, v_inicial_y, d_lim_recta_1, d2, t_lim, f1, f2)
 
 
 plt.figure(figsize=(10, 6))
--- formulas_tp.py
+++ formulas_tp.py
@@ -63,15 +63,21 @@ def runge_kutta_4_orden_superior(f_u, t0, y0, u0, tf, h, condicion_limite):
     
     return t, y, u
 
-def ecuacion_curvas(theta, r, g):
+def ecuacion_recta(tiempo, posicion, velocidad, *args):
+    f = args[0] 
+    if f > FUERZA_MAX:
+        return 0
+    return f / M # Retorna la aceleración (Fuerza / Masa)
+
+def ecuacion_curvas(tiempo, posicion, velocidad, *args):
+    theta, r, g = args[0]
     if g > ACELERACION_MAX:
         return 0
     
     return (- g/ r) * np.sin(theta)
 
 
-def runge_kutta_4_orden_superior_2d(f, t0, x0, y0, vx0, vy0, tf, h, dist_lim):
-    print(f, t0, x0, y0, vx0, vy0, tf, h)
+def runge_kutta_4_orden_superior_2d(f, t0, x0, y0, vx0, vy0, tf, h, dist_lim, *args):
     t = t0
     xs, ys = [x0], [y0]
     vxs, vys = [vx0], [vy0]
@@ -82,29 +88,29 @@ def runge_kutta_4_orden_superior_2d(f, t0, x0, y0, vx0, vy0, tf, h, dist_lim):
     while t < tf and np.sqrt(x**2 + y**2) < dist_lim:
         # k y m para X
         k1x = vx
-        m1x = f(t, x, vx)
+        m1x = f(t, x, vx, *args)
 
         k2x = (vx + (h * m1x)/2)
-        m2x = f(t + h/2, x + (h * k1x)/2, k2x)
+        m2x = f(t + h/2, x + (h * k1x)/2, k2x, *args)
 
         k3x = (vx + (h * m2x)/2)
-        m3x = f(t + h/2, x + (h * k2x)/2, k3x)
+        m3x = f(t + h/2, x + (h * k2x)/2, k3x, *args)
 
         k4x = (vx + (h * m3x)/2)
-        m4x = f(t + h, x + (h * k3x), k4x)
+        m4x = f(t + h, x + (h * k3x), k4x, *args)
 
         # k y m para Y
         k1y = vy
-        m1y = f(t, y, vy)
+        m1y = f(t, y, vy, *args)
 
         k2y = (vy + (h * m1y)/2)
-        m2y = f(t + h/2, y + (h * k1y)/2, k2y)
+        m2y = f(t + h/2, y + (h * k1y)/2, k2y, *args)
 
         k3y = (vy + (h * m2y)/2)
-        m3y = f(t + h/2, y + (h * k2y)/2, k3y)
+        m3y = f(t + h/2, y + (h * k2y)/2, k3y, *args)
 
         k4y = (vy + (h * m3y)/2)
-        m4y = f(t + h, y + (h * k3y), k4y)
+        m4y = f(t + h, y + (h * k3y), k4y, *args)
 
         # Actualización
         x = x + h * (k1x + 2*k2x + 2*k3x + k4x) / 6
@@ -120,3 +126,100 @@ def runge_kutta_4_orden_superior_2d(f, t0, x0, y0, vx0, vy0, tf, h, dist_lim):
         vys.append(vy)
 
     return xs, ys, vxs, vys, t
+
+def runge_kutta_4_orden_superior_2d_no_itera(f, t0, x0, y0, vx0, vy0, h, *args):
+    # k y m para X
+    k1x = vx0
+    m1x = f(t0, x0, vx0, *args)
+
+    k2x = (vx0 + (h * m1x)/2)
+    m2x = f(t0 + h/2, x0 + (h * k1x)/2, k2x, *args)
+
+    k3x = (vx0 + (h * m2x)/2)
+    m3x = f(t0 + h/2, x0 + (h * k2x)/2, k3x, *args)
+
+    k4x = (vx0 + (h * m3x)/2)
+    m4x = f(t0 + h, x0 + (h * k3x), k4x, *args)
+
+    # k y m para Y
+    k1y = vy0
+    m1y = f(t0, y0, vy0, *args)
+
+    k2y = (vy0 + (h * m1y)/2)
+    m2y = f(t0 + h/2, y0 + (h * k1y)/2, k2y, *args)
+
+    k3y = (vy0 + (h * m2y)/2)
+    m3y = f(t0 + h/2, y0 + (h * k2y)/2, k3y, *args)
+
+    k4y = (vy0 + (h * m3y)/2)
+    m4y = f(t0 + h, y0 + (h * k3y), k4y, *args)
+
+    # Actualización
+    x0 = x0 + h * (k1x + 2*k2x + 2*k3x + k4x) / 6
+    vx0 = vx0 + h * (m1x + 2*m2x + 2*m3x + m4x) / 6
+
+    y0 = y0 + h * (k1y + 2*k2y + 2*k3y + k4y) / 6
+    vy0 = vy0 + h * (m1y + 2*m2y + 2*m3y + m4y) / 6
+
+    t0 += h
+
+    return x0, y0, vx0, vy0, t0
+
+def simular_tramo_recto(t0, x0, y0, vx0, vy0, tf, h, dist_lim, *args):
+
+    t = t0
+    xs, ys = [x0], [y0]
+    vxs, vys = [vx0], [vy0]
+
+    x, y = x0, y0
+    vx, vy = vx0, vy0
+
+    while t < tf and np.sqrt(x**2 + y**2) < dist_lim:
+        x, y, vx, vy, t = runge_kutta_4_orden_superior_2d_no_itera(ecuacion_recta, t, x, y, vx, vy, h, *args)
+        xs.append(x)
+        ys.append(y)
+        vxs.append(vx)
+        vys.append(vy)
+
+    return xs, ys, vxs, vys, t
+
+def simular_tramo_curvo(t0, x0, y0, vx0, vy0, tf, h, dist_lim, *args):
+    theta_objetivo, r, g = args[0]
+    angulo_girado = 0.0
+
+    theta = theta_objetivo
+
+    nuevos_args = (0.0, r, g)
+    t = t0
+    xs, ys = [x0], [y0]
+    vxs, vys = [vx0], [vy0]
+
+    x, y = x0, y0
+    vx, vy = vx0, vy0
+    cont = 0
+
+    while t < tf and angulo_girado <=  theta_objetivo :
+        cont += 1
+        x, y, vx, vy, t = runge_kutta_4_orden_superior_2d_no_itera(ecuacion_curvas, t, x, y, vx, vy, h, nuevos_args)
+
+        v = calcular_velocidad_resultante(vx, vy)
+        omega = v / r
+        theta = h*cont*omega 
+        a_lat = v**2 / r
+        angulo_girado = abs(theta_objetivo - theta)
+
+        if a_lat > ACELERACION_MAX:
+            v = np.sqrt(ACELERACION_MAX * r)
+            
+        vx = v * np.cos(theta)
+        vy = v * np.sin(theta)
+        xs.append(x)
+        ys.append(y)
+        vxs.append(vx)
+        vys.append(vy)
+        nuevos_args = (theta, r, g)
+
+    return xs, ys, vxs, vys, t
+
+def calcular_velocidad_resultante(vx, vy):
+     return np.sqrt(vx**2 + vy**2)
